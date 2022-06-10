d980268c277a055515feb5093c043869cfe0353f
Reganvalkyrie <regangirikaruna@gmail.com>
2022-06-10 19:41:31 +0700

    clone from todolist binar task

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { RiCloseCircleLine } from "react-icons/ri";
 import { TiEdit } from "react-icons/ti";
 import TodoForm from "./TodoFrom";
-
+//for challenge 08
 function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
   const [edit, setEdit] = useState({
     id: null,
